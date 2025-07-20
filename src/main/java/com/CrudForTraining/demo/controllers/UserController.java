4d6f7d1a4f86a5a430ae650d7911f95ab32679c7
Gabriel Da Silva <gabrieldasilva.itu2001@gmail.com>
2025-07-20 16:32:49 -0300

    User Controller, Data Base Seeding, test post and get user and some fixes on pom.xml

--- src/main/java/com/CrudForTraining/demo/controllers/UserController.java
+++ src/main/java/com/CrudForTraining/demo/controllers/UserController.java
@@ -1,10 +1,20 @@
 package com.CrudForTraining.demo.controllers;
 
+import java.net.URI;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.CrudForTraining.demo.services.UserService;
+
+import dto.UserDTO;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -13,5 +23,20 @@ public class UserController {
 	
 	@Autowired
 	private UserService service;
+	
+	@GetMapping()
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<UserDTO> list=service.findAll();
+		return ResponseEntity.ok(list);
+		
+	}
+	
+	@PostMapping
+	public ResponseEntity<UserDTO> newUser(@RequestBody UserDTO dto){
+		dto=service.newUser(dto);
+		URI uri=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
 
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_user(name, email) VALUES ('Gabriel', 'gabriel@123');
+INSERT INTO tb_user(name, email) VALUES ('Ga', 'gabriel@456');
+INSERT INTO tb_user(name, email) VALUES ('Wesley', 'wesley@123');
